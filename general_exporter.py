0ad251dc278896665359fa9d2c0c90dfeb4a139c
Conan Weiland <cweiland@bnl.gov>
2025-01-30 13:07:36 -0500

    [CW] bug fixes in switchboard

--- general_exporter.py
+++ general_exporter.py
@@ -1,11 +1,13 @@
 from file_exporter import peak_export
 #from export_tools import get_proposal_path
 from prefect import flow, get_run_logger
+from export_tools import initialize_tiled_client
 
 @flow
 def export_switchboard(uid,beamline_acronym="haxpes"):
     logger = get_run_logger()
-
+    c = initialized_tiled_client(beamline_acronym)
+    run = c[uid]
     if run.start['autoexport']:
         if run.start['scantype'] == "xps":
             peak_export(uid)
