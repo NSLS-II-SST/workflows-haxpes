4608bf953b0f2c30f2f5dd5b647cac3aa2442d72
Conan Weiland <cweiland@bnl.gov>
2025-01-30 13:10:18 -0500

    [CW] bug fixes in switchboard

--- general_exporter.py
+++ general_exporter.py
@@ -6,7 +6,7 @@ from export_tools import initialize_tiled_client
 @flow
 def export_switchboard(uid,beamline_acronym="haxpes"):
     logger = get_run_logger()
-    c = initialized_tiled_client(beamline_acronym)
+    c = initialize_tiled_client(beamline_acronym)
     run = c[uid]
     if run.start['autoexport']:
         if run.start['scantype'] == "xps":
