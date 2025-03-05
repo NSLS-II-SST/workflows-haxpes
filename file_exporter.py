e7c7383d7549b932340febf636d9d1b646ef9c75
Conan Weiland <cweiland@bnl.gov>
2025-03-05 09:47:45 -0500

    [CW] turn header back on; subtle cleanup

--- file_exporter.py
+++ file_exporter.py
@@ -12,19 +12,17 @@ def export_peak_xps(uid, beamline_acronym="haxpes"):
     catalog = initialize_tiled_client(beamline_acronym)
     run = catalog[uid]
 
-    #metadata = get_metadata_xps(run) #temporary turn off header
-    #header = make_header(metadata,"xps") #temporary turn off header
+    metadata = get_metadata_xps(run) 
+    header = make_header(metadata,"xps") 
     data = get_data_xps(run)
     export_path = get_proposal_path(run)+"XPS_export/"
-    #export_path = "/home/xf07id1/Documents/UserFiles/live/LiveData/XPS_export/"
-    #export filename key in md ???
     if not exists(export_path):
         logger.info(f"Export path does not exist, making {export_path}")
         makedirs(export_path)
     filename = export_path+"XPS_scan"+str(run.start['scan_id'])+".csv"
     logger.info("Exporting Peak XPS Data")
-    #np.savetxt(filename,data,delimiter=',',header=header)
-    np.savetxt(filename,data,delimiter=",")
+    np.savetxt(filename,data,delimiter=',',header=header)
+    
 
 @task(retries=2, retry_delay_seconds=10)
 def export_xas(uid, beamline_acronym="haxpes"):
@@ -34,8 +32,8 @@ def export_xas(uid, beamline_acronym="haxpes"):
     run = catalog[uid]
 
     detlist = run.start['detectors']
-    #metadata = get_general_metadata(run) #temporary turn off header
-    #header = make_header(metadata,"xas",detlist=detlist) #temporary turn off header
+    metadata = get_general_metadata(run) 
+    header = make_header(metadata,"xas",detlist=detlist) 
     data = get_xas_data(run)
 
     export_path = get_proposal_path(run)+"XAS_export/"
